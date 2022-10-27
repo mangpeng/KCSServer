ce3599dda72a0d84ceed9ef92cf3d9f6b483a20e
mangpeng <rlatmdwp5@gmail.com>
2022-10-28 07:41:10 +0900

    JobTimer

--- Server/Program.cs
+++ Server/Program.cs
@@ -11,16 +11,25 @@ public partial class Program
     static public IPAddress ipAddr = IPAddress.Parse("127.0.0.1");
     static public IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
+    static void FlushRoom()
+    {
+        Room.Push(()=>Room.Flush());
+        JobTimer.Instance.Push(FlushRoom, 250);
+    }
+    
     public static void Main()
     {
         Listener listener = new Listener();
         listener.Init(endPoint, () => SessionManager.Instance.Generate()); 
         Console.WriteLine("Listening....");
-    
+
+        // todo 더 작은 시간 단위의 jobqueue 어떻게 관리할 것인지 
+        //FlushRoom();
+        JobTimer.Instance.Push(FlushRoom);
+        
         while (true)
         {
-            Room.Push(()=>Room.Flush());
-            Thread.Sleep(250);
+            JobTimer.Instance.Flush();
         }    
     }
 }
